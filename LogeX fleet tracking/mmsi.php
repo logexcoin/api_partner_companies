0c6e6773691bf5ea9d2beb71fbca5c43ebc8a6ee
LogeX dev <wenotine@gmail.com>
2018-02-04 13:30:14 +0100

    get vessel name by imo added

--- LogeX fleet tracking/mmsi.php
+++ LogeX fleet tracking/mmsi.php
@@ -26,18 +26,55 @@ class mmsi{
     protected $callsign = "";
     
 
-    public function getLogexFleet($mmsi){
+    public function getVesselNameByMMSI($mmsi){
+
+        //$this->fleet = $fleet;
+        //fleet for realtime data dev. 
 
         $testFleet = [
             'DELPHIS GDANSK' => 477850500,
+            'NYK OWL' => 431772000,
+            'STAR AMETHYST' => 311000630,
+            'CORATO' => 249431000
+        ];
 
-            
-        ]
+        foreach($testFleet as $key => $tf){
+
+            if(tf == $mmsi){
+                return $key;
+            }
+            else{
+                return null;
+            }
+
+        }
 
 
     }
 
 
-
+    public function getVesselNameByIMO($imo){
+        
+        //todo dev live api $this->imo = $imo;
+                $testFleet = [
+                    'DELPHIS GDANSK' => 9780653,
+                    'NYK OWL' => 9741449,
+                    'STAR AMETHYST' => 9523251,
+                    'CORATO' => 9749829
+                ];
+        
+                foreach($testFleet as $key => $tf){
+        
+                    if(tf == $imo){
+                        return $key;
+                    }
+                    else{
+                        return null;
+                    }
+        
+                }
+        
+        
+            }
 
 }
