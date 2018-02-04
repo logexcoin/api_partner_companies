f9bef8ca986bb0ce6353508c54a57bdb0dc9e18b
LogeX dev <wenotine@gmail.com>
2018-02-04 13:28:58 +0100

    getmmsi by vessel name added

---
+++ LogeX fleet tracking/mmsi.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+*
+*   @here we track the Logex Fleet MMSI numbers
+*
+*/
+
+
+class mmsi{
+
+    protected $freet = [];
+
+    protected $vesselName = "";
+
+    protected $imo = "";
+
+    protected $lat = "";
+
+    protected $long = "";
+    
+    protected $destination = "";
+    
+    protected $flag = "";
+    
+    protected $callsign = "";
+    
+
+    public function getLogexFleet($mmsi){
+
+        $testFleet = [
+            'DELPHIS GDANSK' => 477850500,
+
+            
+        ]
+
+
+    }
+
+
+
+
+}
